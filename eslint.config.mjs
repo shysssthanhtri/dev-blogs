bfe440f595961d3722d1eea73e4794a49a9baf3f
shysssthanhtri <shyssthanhtri@gmail.com>
2025-12-03 18:07:59 +0700

    feat: add import rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import prettier from "eslint-plugin-prettier/recommended";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -15,6 +17,27 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
 ]);
 
 export default eslintConfig;
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  "app/**/*.{js,jsx,ts,tsx}": ["pnpm lint:fix"],
+  "app/**/*.{js,jsx,ts,tsx}": ["pnpm lint"],
 };
