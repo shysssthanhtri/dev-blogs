279eab6f94ca7274e431cf024a083813c0aace5d
shysssthanhtri <shyssthanhtri@gmail.com>
2025-12-04 08:41:54 +0700

    feat: split app sidebar

--- components/app-sidebar/index.tsx
+++ components/app-sidebar/index.tsx
@@ -1,70 +1,14 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
-
+import { AppSidebarContent } from "@/components/app-sidebar/app-sidebar-content";
+import { AppSidebarFooter } from "@/components/app-sidebar/app-sidebar-footer";
 import { AppSidebarHeader } from "@/components/app-sidebar/app-sidebar-header";
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar";
-
-// Menu items.
-const items = [
-  {
-    title: "Home",
-    url: "#",
-    icon: Home,
-  },
-  {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
-  },
-  {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
-];
+import { Sidebar } from "@/components/ui/sidebar";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
       <AppSidebarHeader />
-
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
+      <AppSidebarContent />
+      <AppSidebarFooter />
     </Sidebar>
   );
 }
