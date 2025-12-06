70167469154751af0293844e16966087147bffbe
shysssthanhtri <shyssthanhtri@gmail.com>
2025-12-07 05:50:15 +0700

    feat: implement oath sign in

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,30 +1,9 @@
 import React from "react";
 
-import { SocialLoginGroup } from "@/app/(auth)/_components/social-login-group";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { FieldGroup, FieldSeparator } from "@/components/ui/field";
-
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="bg-auth-light dark:bg-auth-dark bg-cover bg-center bg-no-repeat flex w-full items-center justify-center min-h-svh p-6 md:p-10">
-      <Card className="w-full max-w-sm">
-        <CardHeader>
-          <CardTitle>
-            Join{" "}
-            <span className="italic">
-              Dev <span className="text-primary">Blogs</span>
-            </span>
-          </CardTitle>
-        </CardHeader>
-
-        <CardContent>
-          <FieldGroup>
-            <SocialLoginGroup />
-            <FieldSeparator>Or continue with</FieldSeparator>
-            {children}
-          </FieldGroup>
-        </CardContent>
-      </Card>
+      {children}
     </main>
   );
 };
--- auth.ts
+++ auth.ts
@@ -1,10 +1,17 @@
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 import { mongoClient } from "@/lib/mongo";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: MongoDBAdapter(mongoClient),
-  providers: [GitHub],
+  providers: [
+    GitHub({ allowDangerousEmailAccountLinking: true }),
+    Google({ allowDangerousEmailAccountLinking: true }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
 });
